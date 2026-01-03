5893f32e566e2f62b1d2c1d5ca4fcfe193043a62
Manan-Sanghani <manansanghani7@gmail.com>
2026-01-03 11:37:43 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,41 @@
+#x = 'Hello World'
+print(x)
+
+"""x = 20
+print(x)"""
+
+"""x = 20.5
+print(x)"""
+
+"""x = 1j
+print(x)"""
+
+"""x = ["apple","banana","cherry"]
+print(x)"""
+
+"""x = ("apple","banana","cherry")
+print(x)"""
+
+"""x = range(8)
+print(x)"""
+
+"""x = {"name":"John","age":"36"}
+print(x)"""
+
+"""x = {"apple","banana","cherry"}
+print(x)"""
+
+"""x = True
+print(x)"""
+
+"""X = None
+print(x)"""
+
+"""x = frozenset({"apple","banana","cherry"})
+print(x)"""
+
+x = b"Hello"
+print(x)
+
+x = bytearray(5)
+print(x)
