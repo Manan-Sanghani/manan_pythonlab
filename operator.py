2ba752e66fe912cfc5da414f380bd863c46b2359
Manan-Sanghani <manansanghani7@gmail.com>
2026-01-31 10:26:51 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,72 @@
+#1.Assignment Operator
+
+""" x = 10
+print(x) """
+
+#2.Unary Minus
+
+"""a=5
+print(-a)"""
+
+
+#3.Relational Operator
+
+"""p = 10
+q = 5
+#print(p>q)
+print(p<q)"""
+
+
+#4.Logical operator
+
+"""x = True
+y = False
+#print(x and y)
+#print(x or y)
+#print(not x)"""
+
+
+#5.Boolean Values
+
+"""is_pass=True
+print(is_pass)"""
+
+
+#6.Bitwise operator
+
+"""a=5
+b=3
+#print(a&b)
+print(a|b)"""
+
+
+#7.Membership operator
+
+"""numbers = [1,2,3,4,]
+#print(2 in numbers)
+print(5 not in numbers)"""
+
+
+#8.Identity operator
+
+"""x=10
+y=10
+#print(x is y)
+print(x is not y)"""
+
+
+#9.Arithmetic operator
+
+"""a=10
+b=3
+
+print(a+b)
+print(a%b)"""
+
+
+
+
+
+
+
+
