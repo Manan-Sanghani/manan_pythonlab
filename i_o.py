da0f6deaaf3c68568cb382bad5c3d0ff9f78f862
Manan-Sanghani <manansanghani7@gmail.com>
2026-01-31 10:25:53 +0530

    Add files via upload

---
+++ i_o.py
@@ -0,0 +1,15 @@
+# print("Hello World")
+
+#print("Atmiya University")
+
+#print("Python programming")
+
+
+"""name = input ("Enter your Name : ")
+print("Hello",name)"""
+
+"""age = input("Enter your age :")
+print("My age is",age)"""
+
+"""study = input("Enter your course")
+print("My course is",study)"""
