d024e8a4074f05cc551dc6bbfc7cf565888a8ddb
Manan-Sanghani <manansanghani7@gmail.com>
2026-03-07 11:08:22 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,33 @@
+#EX-1
+
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+    
+#EX-2 print numbers from 1 to 10
+
+i=1
+while i<=10:
+    print(i)
+    i=i+1    
+    
+#EX-3 sum of firsrt n natural numbers
+
+n=int(input("Enter n : "))
+i=1
+s=0
+
+while i<=n:
+    s=s+i
+    i=i+1
+print("sum = ",s)    
+
+#EX-4 Table of a number
+
+num=int(input("Enter number : "))
+i=1
+while i<=10:
+    print(num,"x",i,"=",num *i)
+    i=i+1
+    
