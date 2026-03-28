bf11e2bd63deb44ad7da545d5e0c26032a06a7aa
Manan-Sanghani <manansanghani7@gmail.com>
2026-03-28 11:13:40 +0530

    Add files via upload

---
+++ keywordargument.py
@@ -0,0 +1,18 @@
+# 1.basic keyword argument
+
+def student_info(name,age,city):
+    print("Name :",name)
+    print("Age :",age)
+    print("City :",city)
+    
+student_info(age=18,city="Rajkot",name="Ravi")
+
+
+# 2.mixing positional and keyword
+
+def display(a,b,c):
+    print("a= ",a)
+    print("b= ",b)
+    print("c= ",c)
+    
+display(1,c=3,b=2)
